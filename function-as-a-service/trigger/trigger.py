4f7d865ada649fefecc4022e7da267500b8cd4b6
shubham7jain <shubham7jain@gmail.com>
2018-03-25 02:18:26 -0500

    Fixed a syntax

--- function-as-a-service/trigger/trigger.py
+++ function-as-a-service/trigger/trigger.py
@@ -4,7 +4,7 @@ from resourceManager import ResourceManager
 class Trigger:
 	def __init__(self):
 		self.resourceManager = ResourceManager()
-		self.database = Database
+		self.database = Database()
 
 	def handleRequest(self, payload):
 		source_lambda_id = payload["source_lambda_id"]
