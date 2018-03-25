9f271f1b2240c6a542b25f3f4a6fe1cab71b0902
shubham7jain <shubham7jain@gmail.com>
2018-03-25 02:16:52 -0500

    Added the basic trigger function

---
+++ function-as-a-service/trigger/trigger.py
@@ -0,0 +1,14 @@
+from database import Database
+from resourceManager import ResourceManager
+
+class Trigger:
+	def __init__(self):
+		self.resourceManager = ResourceManager()
+		self.database = Database
+
+	def handleRequest(self, payload):
+		source_lambda_id = payload["source_lambda_id"]
+
+		eventDetails = self.database.findBySourceLambdaId(source_lambda_id)
+
+		
