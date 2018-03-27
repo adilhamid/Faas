ce1b4206450851bdcc332ebf3ca525f4c0143fb5
Abhishek Sharma <mailabhi098@gmail.com>
2018-03-27 17:17:41 -0500

    delete consumer.py

--- source-service/consumer.py
+++
@@ -1,8 +0,0 @@
-from kafka import KafkaConsumer
-
-consumer = KafkaConsumer(bootstrap_servers='localhost:9092')
-consumer.subscribe(['my-topic'])
-
-while True:
-    for message in consumer:
-        print message.value
