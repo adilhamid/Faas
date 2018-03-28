5cf8a2e29fb109fd2e513a97e1b040a851ec688e
adilhamid <aaddil.hamid@gmail.com>
2018-03-28 15:09:45 -0500

    Updated the code to make the listener run

--- function-as-a-service/listener/listener.py
+++ function-as-a-service/listener/listener.py
@@ -1,42 +1,38 @@
 from kafka import KafkaConsumer
-
-import multiprocessing
 import sys
-
 sys.path.append("..")
+
 from config import CONFIG
 from trigger.trigger import Trigger
 
-class Listener(multiprocessing.Process):
+class Listener():
     def __init__(self):
-        multiprocessing.Process.__init__(self)
-        self.stopFlag = multiprocessing.Event()
+        self.stopFlag = False
         self.triggerObj = Trigger()
-        # The KAFKA_QUEUE_HOSTNAME_PORT is coming from config
         self.listenerObj = KafkaConsumer(bootstrap_servers=CONFIG.KAFKA_QUEUE_HOSTNAME_PORT,
                                  auto_offset_reset='earliest',
                                  consumer_timeout_ms=1000)
 
     # Emergency Stop the listener
     def stop(self):
-        self.stopFlag.set()
+        self.stopFlag = True
+
 
     # Run the service to listen to the kafka queue on particular topic
-    def run(self):
-        self.listenerObj.subscribe(['function1'])
+    def startListening(self, topicName):
+        self.listenerObj.subscribe([topicName])
 
-        while not self.stopFlag.is_set():
+        while not self.stopFlag:
             for message in self.listenerObj:
                 print(message)
-                # producer.send('topic', {'key': 'val'})
-                assert isinstance(message.value, dict)
                 # As soon as the request is listened from the Kafka Queue, Invoke the trigger
                 self.triggerObj.handleRequest(self,message.value)
 
-                if self.stopFlag.is_set():
+                if self.stopFlag == True:
                     break
 
         self.listenerObj.close()
 
+# Example Usage
 if __name__ == "__main__":
-    Listener().start()
+    Listener().startListening('function1')
