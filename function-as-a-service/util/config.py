a81f18fb0689574116720c420130f77e81587bdf
adilhamid <aaddil.hamid@gmail.com>
2018-04-18 01:36:26 -0500

    Restructuring the code and removing the cross package referencing bug

---
+++ function-as-a-service/util/config.py
@@ -0,0 +1,11 @@
+
+class Config:
+    def __init__(self):
+        self.KAFKA_QUEUE_HOSTNAME_PORT = "localhost:9092"
+        self.TOPIC_KEYS = ['function1']
+        self.TOPIC_FLAGS = []
+
+        #
+        self.INSTANCE = "ec2-user@ec2-54-89-46-245.compute-1.amazonaws.com"
+
+    
