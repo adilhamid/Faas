9e548d2b389f651662935cca6131a24cd16fe361
Abhishek Sharma <mailabhi098@gmail.com>
2018-04-20 21:18:50 -0500

    fix bug in sending sending data to listner

--- source-service/data-service.py
+++ source-service/data-service.py
@@ -1,6 +1,8 @@
 from flask import Flask, redirect, url_for, request, render_template
 from kafka import KafkaProducer
 import pymongo
+import json
+
 
 app = Flask(__name__)
 
@@ -10,12 +12,13 @@ producer = KafkaProducer(bootstrap_servers='localhost:9092')
 def connect_mongo():
     host = "127.0.0.1:27017"
     db = "faas"
-    collection = "function_topic_mapping"
+    collection = "functions"
     url = "mongodb://" + host + "/" + db
     client = pymongo.MongoClient(url)
     db = client[db]
     collection = db[collection]
     return collection
+
 
 @app.route('/success')
 def success():
@@ -24,10 +27,15 @@ def success():
 
 @app.route('/', methods=['POST', 'GET'])
 def get_data():
-
     if request.method == 'POST':
         task = request.form["task"]
-        producer.send('function1', task.encode('utf-8'))
+        topic = request.form["topic"]
+        data = {
+            "task": task,
+            "topic": topic
+        }
+        message = json.dumps(data)
+        producer.send('function1', message.encode('utf-8'))
         return redirect(url_for('success'))
     collection = connect_mongo()
     topics = collection.distinct('topicName')
